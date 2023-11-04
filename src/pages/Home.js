0ea1ad17a23e30d997003492b37f2462fb315c98
bronjaermin <bronjaermin@gmail.com>
2023-11-04 01:01:31 +0100

    hooks

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
 import HomeCard from "../components/home/HomeCard";
-
-const myTodos = [
-  { id: 1, content: "Ela Programer" },
-  { id: 2, content: "1111111" },
-  { id: 3, content: "2222222" },
-  { id: 4, content: "3333333" },
-  { id: 5, content: "4444444" },
-  { id: 6, content: "5555555" },
-];
+import { MyContext } from "../context/my-context";
+import useMyCustomHook from '../hooks/my-custom-hook'
 
 const Home = () => {
+  const [myTodos, setMyTodos] = useState([
+    { id: 1, content: "Ela Programer" },
+    { id: 2, content: "1111111" },
+    { id: 3, content: "2222222" },
+    { id: 4, content: "3333333" },
+    { id: 5, content: "4444444" },
+    { id: 6, content: "5555555" },
+  ])
+  const [counter, setCounter] = useState(0)
+  const [myInput, setMyInput] = useState('')
+  const programiranje = useContext(MyContext)
+  const mojHuk = useMyCustomHook()
+
+  useEffect(() => {
+  }, [counter])
+
   const myChildToParentFunction = (name, lastName, age) => {
     console.log(name, lastName, age);
   };
 
+  const onIncrementCounter = () => {
+    setCounter(oldValue => oldValue + 1);
+  }
+
+  const onDecrementCounter = () => {
+    setCounter(oldValue => oldValue - 1);
+  }
+
+  console.log("Moje ime je", programiranje.firstName)
+  
   return (
     <div className="content">
       <div className="content-header">
@@ -22,10 +41,14 @@ const Home = () => {
           <h1>MY TODO APP</h1>
         </div>
         <div className="content-header-div  content-header-div-two">
-          <input />
-          <button>Submit</button>
+          <input onChange={(e) => setMyInput(e.target.value)} value={myInput} />
+          <button onClick={() => programiranje.changeFirstName(myInput)}>Submit</button>
         </div>
       </div>
+
+      <button onClick={mojHuk.increment}>Increment</button>
+      <h1>COUNTER: {mojHuk.counter}</h1>
+      <button onClick={mojHuk.decrement}>Decrement</button>
 
       {myTodos.map((myTodo) => (
         <HomeCard
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from "react";
+import "../styles/login.css";
 
 const Login = () => {
-    return <div>Login</div>
-}
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="login-page">
+      <div className="login-page-div">
+        <div className="login-page-input">
+          <label>Username</label>
+          <input
+            type="text"
+            placeholder="Username..."
+            onChange={(e) => setUserName(e.target.value)}
+            value={userName}
+          />
+        </div>
+        <div className="login-page-input">
+          <label>Password</label>
+          <input
+            type="password"
+            placeholder="Password..."
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </div>
+        <div className="login-page-button">
+          <button
+            onClick={() =>
+              console.log("USERNAME, PASSWORD", userName, password)
+            }
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
